be09d8369f547f384672cc80e6fcfdbc45051fa8
Zach Miller <zondex1@gmail.com>
2015-09-23 21:28:40 -0600

    Add Drone movement and Scene1

---
+++ CMC_Project/Assets/Scripts/DroneControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneControl : MonoBehaviour 
+{
+	public float movementSpeed = 1f;
+	public Rigidbody2D thisRigidbody;
+	private Vector2 inputs;
+	private Vector2 inputsSnap;
+	private float dragVal;
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisRigidbody = gameObject.GetComponent<Rigidbody2D>();
+		dragVal = thisRigidbody.drag;
+
+		StartCoroutine("MoveCoroutine");
+	}
+
+	IEnumerator MoveCoroutine()
+	{
+		while(true)
+		{
+			//Debug.Log("X:"+Input.GetAxis("Horizontal")+"  Y:"+ Input.GetAxis("Vertical"));
+			inputs = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+			inputsSnap = new Vector2(Input.GetAxis("HorizontalSnap"), Input.GetAxis("VerticalSnap"));
+
+			if(inputsSnap.magnitude==0)
+			{
+				thisRigidbody.drag = 100f;
+			}
+			else
+			{
+				thisRigidbody.drag = dragVal; 
+				thisRigidbody.AddForce(inputs, ForceMode2D.Impulse);
+			}
+			Debug.Log(thisRigidbody.drag);
+
+			// Clamp max velocity
+			if(thisRigidbody.velocity.magnitude > movementSpeed)
+			{
+				thisRigidbody.velocity = thisRigidbody.velocity.normalized * movementSpeed;
+			}
+			yield return null;
+		}
+	}
+}
